bc69b8a0c08bd05de5417f9ff6729a2fbff4fd80
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2021-11-18 12:11:15 +0100

    added click scroll function to horizontal scroll cards

--- js/scrollingCards.js
+++ js/scrollingCards.js
@@ -9,7 +9,9 @@ async function getCards() {
     const response = await fetch(postsUrl);
     const results = await response.json();
     console.log(results);
-    createPosts(results)
+    createPosts(results);
+    buttonClicks();
+    
   }
   catch(error) {
     console.log(error);
@@ -23,19 +25,34 @@ getCards();
 function createPosts(posts) {
   posts.forEach(function(post) {
     
-    
-
     container.innerHTML += `
           <a class="blog-link" href="/blog-article.html?id=${post.id}">
             <article class="blog-card">
-              <img class="blog-card-image" src="/assets/images/frankie-cordoba-fPYJeMmYWM4-unsplash.jpg" />
+              <img class="blog-card-image" src="${post.featured_image_url}"/>
               <div class="article-details">
-                <address class="author">Jason Gennaro</address>
-                <h4 class="blog-card-title">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h4>
+                <address class="author">${post._embedded.author[0].name}</address>
+                <h4 class="blog-card-title">${post.title.rendered}</h4>
               </div>
             </article>
           </a>`
     ;
       
   })
+}
+
+//Function to create scroll with button clicks.
+
+function buttonClicks() {
+
+  const buttonR = document.querySelector('.fa-chevron-right');
+  const buttonL = document.querySelector('.fa-chevron-left');
+
+  buttonR.onclick = function () {
+    container.scrollLeft += 900;
+  };
+  buttonL.onclick = function () {
+    container.scrollLeft += -900;
+  };
+
+
 }
