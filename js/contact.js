0f5dded39845a38b6f1cbd22fb9e0128e6a414cd
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2021-11-29 14:40:49 +0100

    updated form

--- js/contact.js
+++ js/contact.js
@@ -1,35 +1,64 @@
-const form = document.querySelector('form#contact-form');
+// const form = document.querySelector('form#contact-form');
 
-form.addEventListener('submit', sendForm);
+// form.addEventListener('submit', sendForm);
   
 
-async function sendForm(event) {
-  event.preventDefault();
+// async function sendForm(event) {
+//   event.preventDefault();
 
-  const form = event.target;
+//   const form = event.target;
 
-  try {
+//   try {
 
-    const response = await fetch(form.action, {
+//     const response = await fetch(form.action, {
 
-    method: form.method,
-    body: new FormData(form)
+//     method: form.method,
+//     body: new FormData(form)
     
-  });
-    const data = await response.json();
-    console.log("fetched");
+//   });
+//     const data = await response.json();
+//     console.log("fetched");
 
    
-  }catch(error) {
-    console.warn(error);
+//   }catch(error) {
+//     console.warn(error);
 
-  }
-}
-
-
-// form.onsubmit = function formSuccess() {
-//   form.innerHTML = `
-//                   <div class="success"> Your form was submitted successfully.
-//                   </div>
-//                     `
+//   }
 // }
+
+
+
+
+const formSubmissionHandler = (event) => {
+  event.preventDefault();
+
+  const formElement = event.target,
+    { action, method } = formElement,
+    body = new FormData(formElement);
+
+  fetch(action, {
+    method,
+    body
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      // Determine if the submission is not valid
+      if (isFormSubmissionError(response)) {
+        // Handle the case when there are validation errors
+      }
+      // Handle the happy path
+      formElement.onsubmit = function formSuccess() {
+        formElement.innerHTML = `
+                        <div class="success"> Your form was submitted successfully.
+                        </div>
+                          `
+      }
+    })
+    .catch((error) => {
+      // Handle the case when there's a problem with the request
+    });
+};
+
+const formElement = document.querySelector('form#contact-form');
+
+
