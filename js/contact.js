d03b65bd7ec22c6bca387f364171ce9ea4383093
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2021-11-30 16:42:16 +0100

    updated contact information

--- js/contact.js
+++ js/contact.js
@@ -1,77 +1,78 @@
-const form = document.querySelector('form#contact-form');
+// const form = document.querySelector('form#contact-form');
 
 
-form.addEventListener('submit', sendForm);
+// form.addEventListener('submit', sendForm);
   
 
-async function sendForm(event) {
-  event.preventDefault();
-
-  const form = event.target;
-  const error = document.querySelector('.error-message');
-
-  try {
-
-    const response = await fetch(form.action, {
-
-    method: form.method,
-    body: new FormData(form)
-    
-    
-  });
-  console.log(response);
-    const data = await response.json();
-    
-    console.log("fetched");
-
-    // if(input === invalid) {
-    //   error.display.style = "block";
-    // }
-   
-   
-  }catch(error) {
-    console.warn(error);
-
-  }
-}
-
-
-
-
-
-
-
-// const formSubmissionHandler = (event) => {
+// async function sendForm(event) {
 //   event.preventDefault();
 
-//   const formElement = event.target,
-//     { action, method } = formElement,
-//     body = new FormData(formElement);
+//   const form = event.target;
+//   const error = document.querySelector('.error-message');
 
-//   fetch(action, {
-//     method,
-//     body
-//   })
-//     .then((response) => response.json())
-//     .then((response) => {
-//       // Determine if the submission is not valid
-//       if (isFormSubmissionError(response)) {
-//         // Handle the case when there are validation errors
-//       }
-//       // Handle the happy path
-//       formElement.onsubmit = function formSuccess() {
-//         formElement.innerHTML = `
-//                         <div class="success"> Your form was submitted successfully.
-//                         </div>
-//                           `
-//       }
-//     })
-//     .catch((error) => {
-//       // Handle the case when there's a problem with the request
-//     });
-// };
+//   try {
 
-// const formElement = document.querySelector('form#contact-form');
+//     const response = await fetch(form.action, {
+
+//     method: form.method,
+//     body: new FormData(form)
+    
+    
+//   });
+//   console.log(response);
+//     const data = await response.json();
+    
+//     console.log("fetched");
+
+//     // if(input === invalid) {
+//     //   error.display.style = "block";
+//     // }
+   
+   
+//   }catch(error) {
+//     console.warn(error);
+
+//   }
+// }
+
+
+
+
+
+
+
+const formSubmissionHandler = (event) => {
+  event.preventDefault();
+
+  const formElement = event.target,
+    { action, method } = formElement,
+    body = new FormData(formElement);
+
+  fetch(action, {
+    method,
+    body
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      // Determine if the submission is not valid
+      if (isFormSubmissionError(response)) {
+        // Handle the case when there are validation errors
+        
+          // formElement.innerHTML = `
+          //                 <div class="success"> Your form was submitted successfully.
+          //                 </div>
+          //                   `
+        
+      }
+      // Handle the happy path
+      
+    })
+    .catch((error) => {
+      // Handle the case when there's a problem with the request
+    });
+};
+
+const formElement = document.querySelector('form#contact-form');
 
 
 
